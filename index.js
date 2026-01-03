141a9a43db54bf5d0b2db6ae0372101003e55c00
Your Name <you@example.com>
2026-01-03 14:21:29 +0100

    all files

---
+++ index.js
@@ -0,0 +1,6 @@
+//first add  ShopNow in page produts scroll 
+document.getElementById("ShopNow").addEventListener("click", function() {
+    document.getElementById("Produts").scrollIntoView({ behavior: "smooth" });
+});
+// add To cart
+document.getElementById("")
